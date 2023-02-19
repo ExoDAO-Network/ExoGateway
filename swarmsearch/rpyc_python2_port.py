63dd4b6d7ffeb6145a5128fb1c56fcb5a4652f7b
yzimmermann <92678727+yzimmermann@users.noreply.github.com>
2023-02-19 23:55:44 +0100

    Delete rpyc_python2_port.py

--- swarmsearch/rpyc_python2_port.py
+++
@@ -1,9 +0,0 @@
-import rpyc
-def connect2(ip, port, serviceX):
-    print("try2")
-    c = rpyc.connect(ip, port, service= serviceX, config={'allow_public_attrs': True}) #create clients in all neighboring services
-    return c
-
-def query2(c, querystr):
-    result = c.root.search_query(querystr)
-    return result
